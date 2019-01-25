de1f073259557c3213b79c03cb0a37147e09d7ec
Johannes Müller <mail@gamgee.de>
2019-01-25 21:27:48 +0100

    Added certificate for picoscope driver installation, updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # chocolatey packages
+
+## Development tips
+
+Generate empty package template
+
+```powershell
+choco new packageName
+```
+
+Build and install package from .nuspec file
+
+```powershell
+choco install -fdv C:\PATH\to\.nuspec
+```
+
+Build the package
+
+```powershell
+# run in package directory
+choco pack
+```
+
+Test the package
+
+```powershell
+# run in package directory
+choco install packageName -dv -s .
+```
+
+Push package to cocolatey community feed
+
+```powershell
+choco apikey --key your-api-key --source https://push.chocolatey.org/
+choco push MyPackage.1.0.nupkg --source https://push.chocolatey.org/
+```
+
+## Resources
+
+* [chocolatey.org How to create packages](https://chocolatey.org/docs/create-packages)
+* [vagrant test environment](https://github.com/chocolatey/chocolatey-test-environment)
+* [jsdelivr cdn for icon files](https://www.jsdelivr.com/)
--- picoscope/tools/chocolateyinstall.ps1
+++ picoscope/tools/chocolateyinstall.ps1
@@ -1,41 +1,23 @@
-﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.picotech.com/download/software/sr/PicoScope6_r6_13_15.exe' # download url, HTTPS preferred
+$url        = 'https://www.picotech.com/download/software/sr/PicoScope6_r6_13_15.exe'
+
+$cert = Get-ChildItem Cert:\CurrentUser\TrustedPublisher -Recurse | Where-Object { $_.Thumbprint -eq '‎bae19ab8365e8e08a17df2b42852a4afd81be504' }
+
+if (!$cert) {
+  Start-ChocolateyProcessAsAdmin "certutil -addstore 'TrustedPublisher' '$toolsDir\picotech.cer'"
+}
+
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'EXE' #only one of these: exe, msi, msu
   url           = $url
-  #file         = $fileLocation
-
-  softwareName  = 'picoscope*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-
-  # Checksums are now required as of 0.10.0.
-  # To determine checksums, you can get that from the original site if provided. 
-  # You can also use checksum.exe (choco install checksum) and use it 
-  # e.g. checksum -t sha256 -f path\to\file
+  softwareName  = 'picoscope*'
   checksum      = 'dc391b8f0091e3eadf7f9ba42e911d8d7ee65ec8caf958aafb5014f68a209915'
-  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
-  checksum64    = ''
-  checksumType64= 'sha256' #default is checksumType
-
-  # MSI
-  #silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
-  #validExitCodes= @(0, 3010, 1641)
-  # OTHERS
-  # Uncomment matching EXE type (sorted by most to least common)
-  #silentArgs   = '/S'           # NSIS
-  #silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
-  #silentArgs   = '/s'           # InstallShield
-  silentArgs   = '/s /v"/qn"'   # InstallShield with MSI
-  #silentArgs   = '/s'           # Wise InstallMaster
-  #silentArgs   = '-s'           # Squirrel
-  #silentArgs   = '-q'           # Install4j
-  #silentArgs   = '-s'           # Ghost
-  # Note that some installers, in addition to the silentArgs above, may also need assistance of AHK to achieve silence.
-  #silentArgs   = ''             # none; make silent with input macro script like AutoHotKey (AHK)
-                                 #       https://chocolatey.org/packages/autohotkey.portable
-  validExitCodes= @(0) #please insert other valid exit codes here
+  checksumType  = 'sha256'
+  silentArgs   = '/s /v"/qn"'
+  validExitCodes= @(0)
 }
 
-Install-ChocolateyPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-package
+Install-ChocolateyPackage @packageArgs
