ddbb1b41441c1191c48b25841e080566f4a21348
Johannes Müller <mail@gamgee.de>
2019-11-17 15:28:53 +0100

    updated picoscope to version 6.14.10

--- picoscope/tools/chocolateyinstall.ps1
+++ picoscope/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.picotech.com/download/software/sr/PicoScope6_r6_13_15.exe'
+$url        = 'https://www.picotech.com/download/software/sr/PicoScope6_r6_14_10.exe'
 
 $cert = Get-ChildItem Cert:\CurrentUser\TrustedPublisher -Recurse | Where-Object { $_.Thumbprint -eq '‎bae19ab8365e8e08a17df2b42852a4afd81be504' }
 
@@ -14,7 +14,7 @@ $packageArgs = @{
   fileType      = 'EXE' #only one of these: exe, msi, msu
   url           = $url
   softwareName  = 'picoscope*'
-  checksum      = 'dc391b8f0091e3eadf7f9ba42e911d8d7ee65ec8caf958aafb5014f68a209915'
+  checksum      = 'c6176816fb88f55d18224947a7e9fa681b75879f49bd1dfab2fa948261e9d460'
   checksumType  = 'sha256'
   silentArgs   = '/s /v"/qn"'
   validExitCodes= @(0)
