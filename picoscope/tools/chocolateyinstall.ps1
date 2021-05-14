953be95046e818c46454f90944fc47cb571159c9
Johannes Mueller <mail@gamgee.de>
2021-05-14 13:25:16 +0200

    updated the download url, version and checksum of the picoscope package

--- picoscope/tools/chocolateyinstall.ps1
+++ picoscope/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.picotech.com/download/software/beta/PicoScope6_r6_14_23.exe'
+$url        = 'https://www.picotech.com/download/software/sr/PicoScope6_6.14.44.5870.exe'
 
 $cert = Get-ChildItem Cert:\CurrentUser\TrustedPublisher -Recurse | Where-Object { $_.Thumbprint -eq '‎bae19ab8365e8e08a17df2b42852a4afd81be504' }
 
@@ -14,7 +14,7 @@ $packageArgs = @{
   fileType      = 'EXE' #only one of these: exe, msi, msu
   url           = $url
   softwareName  = 'picoscope*'
-  checksum      = '4c2ebff743398b59320be1508eb7d43d4b9dba079cf5fc30d6467509aa5c6821'
+  checksum      = '06c8a3562ec0bd2cc75708b775671760b78e6a485b358e71fbdca6b6560e8f6f'
   checksumType  = 'sha256'
   silentArgs   = '/s /v"/qn"'
   validExitCodes= @(0)
